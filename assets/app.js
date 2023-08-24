2d261c2068f271197f1a947b9dcb98d3a4fd0ab5
aounhe <>
2023-08-24 11:54:14 +0000

    add an event listener to the search button

--- assets/app.js
+++ assets/app.js
@@ -2,3 +2,5 @@ const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const error404 = document.querySelector('.not-found');
+
+search.addEventListener('click', () => {
