d055657a4d31172ed9f8763ed688336f50b37f89
aounhe <>
2023-08-24 13:43:10 +0000

    declare a variable APIKey and assign it a value call request to the OpenWeatherMap API

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,8 @@
+const apiKey = "key here"
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
-const error404 = document.querySelector('.not-found');
+const error404 = document.getElementById('.not-found');
 
-search.addEventListener('click', () => {
+search.addEventListener('click', () =>
+const url = (city)=> `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
