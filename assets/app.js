b1b1ee19e271da421129b8a89079f3adf9609ba7
aounhe <>
2023-08-24 13:54:12 +0000

    fetch by city request to the OpenWeatherMap API
     retriev responce and convert to JSON format using the .json() method

--- assets/app.js
+++ assets/app.js
@@ -1,8 +1,17 @@
-const apiKey = "key here"
+const apiKey = "key here";
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 const error404 = document.getElementById('.not-found');
 
-search.addEventListener('click', () =>
-const url = (city)=> `https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}')
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
+
+async function getWeatherByLocation(city){
+     
+    const resp = await fetch(url(city), {
+        origin: "cros" });
+    const respData = await resp.json();
+
+      addWeatherToPage(respData);
+     
+}
