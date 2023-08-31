e12a96bf72c7c6d04d3491fd41aea1c7be9a3b24
aounhe <>
2023-08-31 08:18:31 +0000

    making a request and fetching a resource from api

--- assets/app.js
+++ assets/app.js
@@ -2,4 +2,15 @@ const apiKey = "fdd6234a0da67e8896c2790856616d04";
 const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
+const url = (city)=> `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
 
+ 
+async function getWeatherByLocation(city){
+     
+    const resp = await fetch(url(city), {
+        origin: "cros" });
+    const respData = await resp.json();
+
+      addWeatherToPage(respData);
+     
+}
